0c71507afd2fee8af3853f72d6303a8390e33a27
npaul008 <npaulemon@gmail.com>
2023-12-20 13:07:00 -0500

    remove un-needed dependency import in DemoApplication.java

--- src/main/java/com/byte_transfer/demo/DemoApplication.java
+++ src/main/java/com/byte_transfer/demo/DemoApplication.java
@@ -2,7 +2,6 @@ package com.byte_transfer.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.byte_transfer.demo.controller.MainController;
 
 @SpringBootApplication
 public class DemoApplication {
