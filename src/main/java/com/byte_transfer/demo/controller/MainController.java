02eb6a1ea1e4491e4e753d2dd8bdf8adefa896cd
npaul008 <npaulemon@gmail.com>
2023-12-20 20:00:37 -0500

    remove whitespace from MainController.java

--- src/main/java/com/byte_transfer/demo/controller/MainController.java
+++ src/main/java/com/byte_transfer/demo/controller/MainController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
-
 @RestController
 @RequestMapping(value ="/", method=RequestMethod.GET)
 public class MainController {
